425495bc69a20ff41a775873f81c088eb723a993
ieiieiiei2211-dev <ieiieiiei2211@gmail.com>
2026-04-24 20:22:51 +0900

    add user creation and validation

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
   findAll() {
-    return this.prisma.user.findMany({
-      orderBy: { id: 'asc' },
+    return this.prisma.user.findMany();
+  }
+
+  create(createUserDto: CreateUserDto) {
+    return this.prisma.user.create({
+      data: {
+        name: createUserDto.name,
+        email: createUserDto.email,
+      },
     });
   }
 }
